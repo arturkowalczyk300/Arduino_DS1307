4ac731a387eb2984cfbddd03d0da7b9623fc1df5
Artur <artur389983@gmail.com>
2020-02-23 13:12:01 +0100

    add prototypes

--- Arduino_DS1307.cpp
+++ Arduino_DS1307.cpp
@@ -1,1 +1,34 @@
 #include "Arduino_DS1307.h"
+
+bool testConnection()
+{
+		return false;
+}
+
+
+TIME_DATE_STRUCT readTime()
+{
+	return TIME_DATE_STRUCT(12, 30, 59, 0, 0, 0);
+}
+TIME_DATE_STRUCT readDate()
+{
+	return TIME_DATE_STRUCT(0, 0, 0, 2, 6, 1997);
+}
+TIME_DATE_STRUCT readTimeDate()
+{
+	return TIME_DATE_STRUCT(12, 30, 59, 2, 6, 1997);
+}
+
+bool writeTime(TIME_DATE_STRUCT time)
+{
+	return false;
+}
+bool writeDate(TIME_DATE_STRUCT date)
+{
+	return false;
+}
+bool writeTimeDate(TIME_DATE_STRUCT timeDate)
+{
+	return false;
+	
+}
--- Arduino_DS1307.h
+++ Arduino_DS1307.h
@@ -2,7 +2,30 @@
 #define ARDUINO_DS1307_H
 
 #include <Arduino.h>
+#include <Wire.h>
 
+struct TIME_DATE_STRUCT
+{
+	unsigned int hours;
+	unsigned int minutes;
+	unsigned int seconds;
+	unsigned int hour;
+	unsigned int day;	
+	unsigned int month;
+	unsigned int year;
+		
+	TIME_DATE_STRUCT(unsigned int hours, unsigned int minutes, unsigned int seconds);
+	TIME_DATE_STRUCT(unsigned int hours, unsigned int minutes, unsigned int seconds, unsigned int day, unsigned int month, unsigned int year );
+	
+};
 
+bool testConnection();
+TIME_DATE_STRUCT readTime();
+TIME_DATE_STRUCT readDate();
+TIME_DATE_STRUCT readTimeDate();
+
+bool writeTime(TIME_DATE_STRUCT time);
+bool writeDate(TIME_DATE_STRUCT date);
+bool writeTimeDate(TIME_DATE_STRUCT timeDate);
 
 #endif
